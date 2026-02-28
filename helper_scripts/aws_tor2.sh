c0c529480435be4b569be3fff0fc1f8f5860d4ee
Dennislee:) <55692615+dennislee928@users.noreply.github.com>
2026-02-28 12:01:26 +0800

    Update aws_tor2.sh

---
+++ helper_scripts/aws_tor2.sh
@@ -0,0 +1,16 @@
+# 1) 安裝（如果還沒裝）
+brew install awscli
+# 2) 設定憑證（只在本次 terminal 有效）
+
+
+
+export AWS_ACCESS_KEY_ID="R2_ACCESS_KEY_ID"
+export AWS_SECRET_ACCESS_KEY="R2_SECRET_ACCESS_KEY"
+export AWS_DEFAULT_REGION="auto"
+# 3) 上傳 23GB zip 到你指定的 prefix
+aws s3 cp "europe_gopro.zip" \
+  "s3://personal-log-datas/europe_gopro_2026/europe_gopro.zip" \
+  --endpoint-url "https://8dfc8c4994bd0925c72ab9e2eff79b48.r2.cloudflarestorage.com"
+# 4) 驗證檔案已在桶內
+aws s3 ls "s3://personal-log-datas/europe_gopro_2026/" \
+  --endpoint-url "https://8dfc8c4994bd0925c72ab9e2eff79b48.r2.cloudflarestorage.com"
